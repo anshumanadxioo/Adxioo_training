d722805189dd5d64b37b8caa66ab405e94ff187b
anshumanadxioo <Anshuman.Mishra@adxioo.com>
2024-06-20 02:20:41 -0700

    Initial commit

---
+++ temp.js
@@ -0,0 +1,15 @@
+ let i=10;
+ var z=20;
+ let x=50;
+function myName(){
+    const name="anshuman";
+    console.log(name);
+}
+myName();
+if(!Math.truncn){
+    Math.truncn=function (num){
+        return num>0? Math.ceil(num):Math.floor(num);
+    }
+}
+console.log(Math.truncn(54.34));
+
